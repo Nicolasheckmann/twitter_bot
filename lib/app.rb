92af004a0762cca6bdade35a9b74090b3655d4ae
Nicolasheckmann <nicolas.heckman@gmail.com>
2021-04-21 14:11:41 +0200

    added a bot that says hello

---
+++ lib/app.rb
@@ -0,0 +1,16 @@
+require 'twitter'
+require 'dotenv'
+require 'pry'
+
+Dotenv.load
+
+def login_twitter()
+    client = Twitter::REST::Client.new do |config|
+        config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
+        config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
+        config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+        config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
+    end
+end
+
+login_twitter().update('Mon premier tweet en Ruby !!!!')
